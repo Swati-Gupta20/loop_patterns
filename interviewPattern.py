24a9623b35891d0c49c1b7cd55d8fd7b87381a30
Swati-Gupta20 <106022280+Swati-Gupta20@users.noreply.github.com>
2022-10-21 11:36:42 +0530

    Add files via upload

--- interviewPattern.py
+++ interviewPattern.py
@@ -22,13 +22,15 @@
 #     i+=1
 
 
-# n=int(input("enter rows:-"))
-# for i in range(n):
-#     for j in range(i+1):
-#         print(j+1,end=" ")
-#     for j in range(i):
-#         print(i-j,end=" ")
-#     print()
+n=int(input("enter rows:-"))
+for i in range(n):
+    for j in range(n-i):
+        print(' ',end=' ')
+    for j in range(i+1):
+        print(j+1,end=" ")
+    for j in range(i):
+        print(i-j,end=" ")
+    print()
 
 
 
--- numberPyramidPattern.py
+++ numberPyramidPattern.py
@@ -9,12 +9,27 @@
 
 
 
-n=int(input("enter rows:-"))
-for i in range(n):
-    for j in range(n-i):
-        print(" ",end=" ")
-    for j in range(i):
-        print(i-j+1,end=" ")
-    for j in range(i+1):
-        print(j+1,end=" ")
+# n=int(input("enter rows:-"))
+# for i in range(n):
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for j in range(i):
+#         print(i-j+1,end=" ")
+#     for j in range(i+1):
+#         print(j+1,end=" ")
+#     print()
+
+
+
+i=1
+while i<=5:
+    k=1
+    while k<=5-i:
+        print(' ',end=' ')
+        k+=1
+    j=1
+    while j<=i:
+        print(i-j+1,end=' ')
+        j+=1
+    i+=1
     print()
